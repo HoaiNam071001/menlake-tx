c5ab298f1772d265e690ad31305c4448ba425abd
Huy Dinh <huy.dinh@codebox.vn>
2022-11-28 23:12:21 +0700

    Init project and dependencies

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { CollapseDirective, CollapseModule } from 'ngx-bootstrap/collapse';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+const angularModules = [
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+  RouterModule,
+];
+
+const libraryModules = [
+  BsDatepickerModule,
+  BsDropdownModule,
+  AlertModule,
+  TimepickerModule,
+  ButtonsModule,
+  ModalModule,
+  TooltipModule,
+  PopoverModule,
+  TabsModule,
+  AccordionModule,
+  CollapseModule,
+];
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    ...angularModules,
+    ...libraryModules,
+  ],
+  providers: [
+  ],
+  exports: [
+    ...angularModules,
+    ...libraryModules,
+  ],
+})
+export class SharedModule { }
