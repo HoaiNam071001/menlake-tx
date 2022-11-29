6c7f51f3098a386e48a3fd45cada43f6e519fb10
Huy Dinh <huy.dinh@codebox.vn>
2022-11-29 21:16:24 +0700

    TX: Add components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { TxTableComponent } from './_components/tx-table/tx-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TxTableComponent
   ],
   imports: [
     BrowserModule,
